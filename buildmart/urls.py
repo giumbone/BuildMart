fb9dbd2570084d475a8f79e4cbf0faeb06b39096
giumbone <johnnydeep29485@gmail.com>
2024-08-06 17:54:22 +0300

    change variable

--- buildmart/urls.py
+++ buildmart/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
+# URL configurations for the BuildMart project
 urlpatterns = [
+    # Admin site URLs
     path('admin/', admin.site.urls),
+    
+    # Include URL configurations from the shop application
     path('shop/', include('shop.urls')),
+    # The 'shop.urls' module should define paths specific to the shop application,
+    # such as listing products, viewing product details, etc.
 ]
