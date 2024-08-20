fabfa18e200523560af542b3c948c30437d3b148
giumbone <johnnydeep29485@gmail.com>
2024-08-20 22:11:43 +0300

    added new function

--- shop/admin.py
+++ shop/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from .models import Product
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'stock')
-    search_fields = ('name',)
+    list_display = ('name', 'price', 'stock', 'created_at')
+    list_editable = ('price', 'stock')
+    search_fields = ('name', 'price')
+    list_filter = ('price', 'stock')
+    date_hierarchy = 'created_at' 
+    prepopulated_fields = {"slug": ("name",)}
+    list_per_page = 20
 
 admin.site.register(Product, ProductAdmin)
