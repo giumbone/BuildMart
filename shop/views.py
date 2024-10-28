694a24fde843d5164890fe484df4f09fa83a9eb6
giumbone <johnnydeep29485@gmail.com>
2024-10-29 01:38:52 +0300

    new update

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 import os
 
 app = Flask(__name__)
@@ -8,20 +8,41 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 app.config['SECRET_KEY'] = SECRET_KEY
 
+products_db = [
+    {"id": 1, "name": "Product 1", "price": 100},
+    {"id": 2, "name": "Product 2", "price": 200}
+]
+
 @app.route('/')
 def home():
-    products = [{"name": "Product 1", "price": 100}, {"name": "Product 2", "price": 200}]
-    return render_template('index.html', products=products)
+    return render_template('index.html', products=products_db)
 
 @app.route('/product/<int:product_id>')
 def product(product_id):
-    product = {"id": product_id, "name": f"Product {product_id}", "description": "Product Description", "price": 100*product_id}
-    return render_template('product_detail.html', product=product)
+    product = next((item for item in products_db if item["id"] == product_id), None)
+    if product:
+        return render_template('product_detail.html', product=product)
+    else:
+        return "Product not found", 404
 
 @app.route('/add_product', methods=['GET', 'POST'])
 def add_product():
     if request.method == 'POST':
-        return redirect(url_for('home'))
+        new_product_name = request.form.get('product_name')
+        new_product_price = request.form.get('product_price')
+        
+        if new_product_name and new_product_price:
+            new_product_id = len(products_db) + 1
+            products_db.append({
+                "id": new_product_id,
+                "name": new_product_name,
+                "price": float(new_product_price)
+            })
+            flash('New product added successfully!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Product name and price are required!', 'error')
+        
     return render_template('add_product.html')
 
 if __name__ == '__main__':
